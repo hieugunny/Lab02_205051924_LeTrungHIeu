6d69d4309e5b836197f9a3338e992020aaea2907
hieugunny <hieult420@uef.edu.vn>
2023-03-07 02:49:32 +0700

    Add project files.

---
+++ Lab02_205051924_LeTrungHIeu/Controllers/MoviesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab02_205051924_LeTrungHIeu.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab02_205051924_LeTrungHIeu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab02_205051924_LeTrungHIeu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
